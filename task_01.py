3c7c50aa6939aa7bdb83ca040d85c83f7a79b575
VladislavBah <122110831+VladislavBah@users.noreply.github.com>
2023-01-07 14:55:27 +0300

    Добавил файл task_01 с решением

---
+++ task_01.py
@@ -0,0 +1,7 @@
+# Задача 1
+#https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/python
+def past(h, m, s):
+    ms = int(3600000 * h + 60000 * m + 1000 * s)
+    return ms
+
+print(past(0,1,1))
